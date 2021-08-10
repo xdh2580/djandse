153d41bbd40c0d4f3f5a181bbd9e28963d0ddd35
xdh2580 <1546741822@qq.com>
2021-08-10 18:26:39 +0800

    从第三方爬取豆瓣top电影名

--- HelloWorld/HelloWorld/mselenium.py
+++ HelloWorld/HelloWorld/mselenium.py
@@ -14,27 +14,53 @@ chrome_options = Options()
 browser = webdriver.Chrome(
     r'C:\Users\XDH\PycharmProjects\seleniumfirst\venv\Scripts\chromedriver.exe',
     options=chrome_options)  # 获取chrome浏览器的驱动，并启动Chrome浏览器
-# 打开百度
-browser.get('https://www.baidu.com/')
-# 等待加载，最多等待20秒
-browser.implicitly_wait(20)
-browser.maximize_window()  # 窗口最大化
 
-su = browser.find_element_by_id("su")
-kw = browser.find_element_by_id("kw")
-su_content = su.get_attribute("value")
-print(su_content)
 
-browser.maximize_window()
-browser.set_script_timeout(5)
-kw.send_keys("selenium")
-su.click()
-# browser.refresh()
-# browser.execute_script("alert('hello')")
-browser.back()
-browser.quit()
+def baidu():
+    # 打开百度
+    browser.get('https://www.baidu.com/')
+    # 等待加载，最多等待20秒
+    browser.implicitly_wait(20)
+    browser.maximize_window()  # 窗口最大化
 
-# root=tkinter.Tk()
+    su = browser.find_element_by_id("su")
+    kw = browser.find_element_by_id("kw")
+    su_content = su.get_attribute("value")
+    print(su_content)
+
+    browser.maximize_window()
+    browser.set_script_timeout(5)
+    kw.send_keys("selenium")
+    su.click()
+    # browser.refresh()
+    # browser.execute_script("alert('hello')")
+    browser.back()
+    browser.execute_script("alert('浏览器将在3秒后自动关闭')")
+    time.sleep(3)
+    browser.quit()
+
+    # root=tkinter.Tk()
+
+
+def douban():
+    browser.get('https://www.km.com/piandan/1912.html?page=1')
+    # 等待加载，最多等待20秒
+    browser.implicitly_wait(20)
+    browser.maximize_window()  # 窗口最大化
+    a = browser.find_element_by_class_name("m-t-t")
+    b = browser.find_elements_by_class_name("m-t-t")
+    print(a.text)
+    for i in b:
+        print(i.text)
+
+
 
 def get_something_fome_selenium():
-    return su_content
+    # return su_content
+    return "something"
+
+
+douban()
+browser.execute_script("alert('浏览器将在3秒后自动关闭')")
+time.sleep(3)
+browser.quit()
