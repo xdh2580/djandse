6011b876c99b5a61e9fdd50cd0df96fb20e043e3
xdh2580 <1546741822@qq.com>
2021-08-11 13:31:21 +0800

    selenium爬取电影名-top250,并使用django网页展示

--- HelloWorld/HelloWorld/mselenium.py
+++ HelloWorld/HelloWorld/mselenium.py
@@ -8,6 +8,7 @@ import time
 
 # 设置selenium使用chrome的无头模式
 chrome_options = Options()
+movieList = []
 
 # chrome_options.add_argument('headless')  # 设置option,隐藏浏览器界面
 # 在启动浏览器时加入配置
@@ -42,24 +43,54 @@ def baidu():
     # root=tkinter.Tk()
 
 
+def find_movies_name_in_current_page():
+    a = browser.find_element_by_class_name("m-t-t")
+    b = browser.find_elements_by_class_name("m-t-t")
+    # print(a.text)
+    for i in b:
+        movieList.append(i.text)
+        print(i.text)
+
+
 def douban():
     browser.get('https://www.km.com/piandan/1912.html?page=1')
     # 等待加载，最多等待20秒
     browser.implicitly_wait(20)
     browser.maximize_window()  # 窗口最大化
-    a = browser.find_element_by_class_name("m-t-t")
-    b = browser.find_elements_by_class_name("m-t-t")
-    print(a.text)
-    for i in b:
-        print(i.text)
+
+
+    while True:
+        pages = browser.find_element_by_class_name("v_page")
+        try:
+            browser.implicitly_wait(5)
+            find_movies_name_in_current_page()
+            next_page = pages.find_element_by_xpath("//*[text()='下一页']")
+            next_page.click()
+        except BaseException as e:
+            # raise e # 如果raise异常，程序会直接停止并打印异常信息
+            print("异常，未找到“下一页”，已是最后一页")
+            break
+
+
+    # for i in pages:
+    #     print("D:i-"+i.text)
+    #     if i.text == "下一页":
+    #         print("D：有下一页标签")
+
 
 
 def get_something_from_selenium():
     # return su_content
-    return "something"
+    all_in_one_string = ""
+    for i in movieList:
+        all_in_one_string = all_in_one_string+"\t"+i
+
+    return all_in_one_string
 
 
 douban()
 browser.execute_script("alert('浏览器将在3秒后自动关闭')")
 time.sleep(3)
 browser.quit()
+# print("------------------------------------")
+# print(get_something_from_selenium())
